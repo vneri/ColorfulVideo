14f77174f22a476ecffa034079b9b5f135f0063f
Valerio <valerioneri.de@gmail.com>
2019-09-09 10:05:17 +0200

    Created the file first time

---
+++ ColorfulVideo.js
@@ -0,0 +1,79 @@
+// Library for changing background colors by time of a HTML 5 video
+// Create stunning fullscreen color effects
+// published under MIT license, Valerio Neri
+
+/*
+		How to use
+		----------
+		In the animation sequence 
+		var animationSequence=[];
+		animationSequence[21] = '#451080';
+		animationSequence[25] = '#452080';
+		animationSequence[27] = '#085EA0';
+		animationSequence[29] = '#AAE580';
+
+		var targetElement = document.getElementById('background');
+		var videoElement = document.getElementById('video');
+		var colorAttribute = 'background';
+		
+		
+
+*/
+
+
+var ColorfulVideo={
+	targetElement:"",
+	videoElement:"",
+	colorAttribute:"",
+	animationSequence:"",
+	startValue:"",
+	animationKeyPoints:"",
+	
+	start: function(options){
+		ColorfulVideo.targetElement = options.targetElement;
+		ColorfulVideo.videoElement = options.videoElement;
+		ColorfulVideo.colorAttribute = options.colorAttribute;
+		ColorfulVideo.animationSequence = options.animationSequence;
+		ColorfulVideo.startValue = ColorfulVideo.targetElement.style[colorAttribute];
+		// set black if empty
+		if (ColorfulVideo.startValue == ''){
+			ColorfulVideo.startValue = '#0000';
+		}
+		// set a start value if it was not set
+		if (ColorfulVideo.animationSequence[0] == undefined){
+			ColorfulVideo.animationSequence[0] = ColorfulVideo.startValue;
+		}
+		ColorfulVideo.animationKeyPoints = Object.keys(ColorfulVideo.animationSequence).sort(ColorfulVideo.compareNumbers);
+		// check the video
+		videoElement.addEventListener("timeupdate", ColorfulVideo.watchTheTime, true);
+	},
+	
+	watchTheTime: function(){
+		// check the time and set the color
+		var actualValue = ColorfulVideo.animationSequence[Math.round(ColorfulVideo.videoElement.currentTime.toFixed(2) * 1e2) / 1e2];
+		if (actualValue == undefined ){
+			// search the nearest to the actual time and get that value
+			var targetIndex = ColorfulVideo.animationKeyPoints.find(
+				function(el){ 
+					return el <= Math.round(ColorfulVideo.videoElement.currentTime.toFixed(2) * 1e2) / 1e2;
+				});
+			if (targetIndex != undefined){
+				actualValue = ColorfulVideo.animationSequence[targetIndex];
+			} else {
+			// if no value available, take the start value
+				actualValue = ColorfulVideo.startValue;
+			}
+		}
+		ColorfulVideo.targetElement.style[ColorfulVideo.colorAttribute] = actualValue;
+	},
+	
+	showExactTime:function(){
+		videoElement.addEventListener("timeupdate", function(){console.log(ColorfulVideo.videoElement.currentTime.toFixed(2));}, true);
+	},
+	
+	compareNumbers: function(a,b){
+		// sort in ascending order
+		return b - a;
+	}
+	
+}
